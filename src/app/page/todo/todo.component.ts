99ebfc2e3e8772d7679b4a7a2ae119125fcb3477
lagunimzz <lagunimzz@gmail.com>
2020-12-06 10:56:48 +0700

    add models

--- src/app/page/todo/todo.component.ts
+++ src/app/page/todo/todo.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  styleUrls: ['./todo.component.css'],
 })
-export class TodoComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class TodoComponent implements OnInit, OnDestroy {
+  constructor() {}
+  ngOnDestroy(): void {
+    console.log('on destroy');
   }
 
+  ngOnInit(): void {
+    console.log('on init');
+  }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  endPointUrl: 'https://jsonplaceholder.typicode.com/prod',
 };
