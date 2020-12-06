d823470c56b0d5f0ba3cf4cddfa1ddc1b4115239
lagunimzz <lagunimzz@gmail.com>
2020-12-06 09:34:40 +0700

    change aa => initsoft angular day 2

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'aa'`, () => {
+  it(`should have as title 'initsoft_angular_day_2'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('aa');
+    expect(app.title).toEqual('initsoft_angular_day_2');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('aa app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain(
+      'initsoft_angular_day_2 app is running!'
+    );
   });
 });
