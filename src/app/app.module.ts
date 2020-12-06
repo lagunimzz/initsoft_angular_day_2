d95b3a72ece5bc439fdd8f8521e7a00553ee01ba
lagunimzz <lagunimzz@gmail.com>
2020-12-06 15:34:31 +0700

    add rtdb

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { environment } from './../environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -10,6 +11,9 @@ import { TodoComponent } from './page/todo/todo.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { HttpClientModule } from '@angular/common/http';
 import { MatListModule } from '@angular/material/list';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { MatInputModule } from '@angular/material/input';
 @NgModule({
   declarations: [AppComponent, HeaderComponent, TodoComponent, PostComponent],
   imports: [
@@ -19,6 +23,9 @@ import { MatListModule } from '@angular/material/list';
     MatToolbarModule,
     HttpClientModule,
     MatListModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireDatabaseModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/page/post/post.component.ts
+++ src/app/page/post/post.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.css']
+  styleUrls: ['./post.component.css'],
 })
 export class PostComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  post: AngularFireList<any>;
+  posts: any;
+  constructor(private db: AngularFireDatabase) {
+    this.post = db.list('/post');
   }
 
+  addPost(event: any): void {
+    if (event.key === 'Enter') {
+      this.post.push(event.target.value);
+      event.target.value = '';
+    }
+  }
+
+  ngOnInit(): void {
+    this.post.valueChanges().subscribe((post) => {
+      this.posts = post;
+    });
+  }
 }
